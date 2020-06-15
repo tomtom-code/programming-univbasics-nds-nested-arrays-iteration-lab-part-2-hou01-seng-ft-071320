6a1ff3528c95553c40a10064b1161a4860ea2779
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-15 17:59:40 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,25 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
-end
+  
+  row_index = 0 
+  outer_result = [] 
+  
+  while row_index < src.length do
+    min_in = 100
+    element_index = 0
+    while element_index < src[row_index].count do
+      
+      if src[row_index][element_index] < min_in
+        min_in = src[row_index][element_index]
+     end
+    
+    
+    element_index += 1 
+   end
+  
+  outer_result << min_in
+  row_index += 1 
+  end
+  outer_result
+end 
